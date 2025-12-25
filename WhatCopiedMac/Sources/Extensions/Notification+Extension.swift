35c52c4d436f0b97f7f036e232307e23e2c0c6ea
Ying Zhong <0x00eeee@gmail.com>
2025-12-25 09:49:22 +0800

    Improve pasteboard observing (#2)

---
+++ WhatCopiedMac/Sources/Extensions/Notification+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  Notification+Extension.swift
+//  WhatCopied
+//
+//  Created by cyan on 2025/12/25.
+//
+
+import AppKit
+
+extension Notification.Name {
+  static let pasteboardChanged = Self("pasteboardChanged")
+}
